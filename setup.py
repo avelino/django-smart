02ded556c5539c2488776ed1b1a91ac46094e1df
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-07-22 00:54:14 -0300

    create setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-smart',
+    version='0.1',
+    description='A smartadmin skin for the Django Admin-Interface.',
+    long_description=read('README.rst'),
+    url='https://github.com/avelino/django-smart',
+    download_url='',
+    author='Thiago Avelino',
+    author_email='thiago@avelino.xxx',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+    zip_safe=False,
+)
